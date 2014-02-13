a3a70070577b40c62ade37e8056fecf205526a0b
Tomosuke.St <tmskst@gmail.com>
2014-02-13 21:28:35 +0900

    update BitmapInstance
    継承関係を追加

--- jsfl/hxclasses/jsfl/BitmapInstance.hx
+++ jsfl/hxclasses/jsfl/BitmapInstance.hx
@@ -1,13 +1,13 @@
 package jsfl;
 
 @:native("BitmapInstance")
-extern class BitmapInstance {
+extern class BitmapInstance extends Instance {
 	
 	// ビットマップのビットからビットマップエフェクトを作成し、操作し、Flash に戻します。
-	public function getBits(): { width:Float, height:Float, depth:Int, bits:Array<Float>, ?cTab:Array<String> };
+	public function getBits():Bits;
 	
 	// 既存のビットマップエレメントのビットを設定します。
-	public function setBits(bitmap: { width:Float, height:Float, depth:Int, bits:Array<Float>, ?cTab:Array<String> } ):Void;
+	public function setBits(bitmap:Bits):Void;
 	
 	// 読み取り専用。ビットマップの幅を表すピクセル単位の整数です。
 	public var hPixels(default, null):Int;
@@ -15,4 +15,12 @@ extern class BitmapInstance {
 	// 読み取り専用。ビットマップの高さを表すピクセル単位の整数です。
 	public var vPixels(default, null):Int;
 	
+}
+
+typedef Bits = {
+	var width:Int;
+	var height:Int;
+	var depth:Int;
+	var bits:Array<Float>;
+	@:optional var cTab:Array<String>;
 }
